d6af72f38865b6ee7feca426e0fa924edc63fef0
richer-tan <155669191+richer-tan@users.noreply.github.com>
2024-02-09 15:54:37 -0500

    testing product_details

--- hello.py
+++ hello.py
@@ -11,8 +11,8 @@ def products():
     return '<h1>Product Page</h1>'
 
 @app.route('/products/<string:id>')
-def product_select(id):
-    return f'Id of this page is {id}'
+def product_detail(id):
+    return f'<h1>Product #{id} Details</h1>'
 
 if __name__ == '__main__': 
     app.run(host='0.0.0.0', port=80)
