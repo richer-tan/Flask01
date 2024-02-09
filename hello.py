a568f37c700d7149779971a8f89f0777c434e908
richer-tan <155669191+richer-tan@users.noreply.github.com>
2024-02-09 15:44:38 -0500

    adds a new endpoint (product page)

--- hello.py
+++ hello.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route('/products')
+def products():
+    return '<h1>Product Page</h1>'
+
 if __name__ == '__main__': 
     app.run(host='0.0.0.0', port=80)
+
+
